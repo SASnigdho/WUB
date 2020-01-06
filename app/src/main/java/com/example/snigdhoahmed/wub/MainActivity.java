9f4bcaa673f1b2514300b36eb591a26020b812f4
Snigdho Ahmed <salmanahmedims@gmail.com>
2020-01-06 17:01:09 +0600

    Update To Androidx.

--- app/src/main/java/com/example/snigdhoahmed/wub/MainActivity.java
+++ app/src/main/java/com/example/snigdhoahmed/wub/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.snigdhoahmed.wub;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
